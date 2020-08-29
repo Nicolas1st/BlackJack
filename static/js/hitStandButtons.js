b3dbca975430276a0e62a9eede2725a6c3fb391b
Nicolas <nicolashereigo@gmail.com>
2020-08-29 23:17:17 +0300

    Changes in js files

--- static/js/hitStandButtons.js
+++ static/js/hitStandButtons.js
@@ -1,21 +1,22 @@
+let cardImgs = ['0_10.jpg', '10_3.jpg', '11_3.jpg', '12_4.jpg', '13_4.jpg', '14_4.jpg', '15_4.jpg', '16_5.jpg', '17_5.jpg', '18_5.jpg', '19_5.jpg', '1_10.jpg', '20_6.jpg', '21_6.jpg', '22_6.jpg', '23_6.jpg', '24_7.jpg', '25_7.jpg', '26_7.jpg', '27_7.jpg', '28_8.jpg', '29_8.jpg', '2_10.jpg', '30_8.jpg', '31_8.jpg', '32_9.jpg', '33_9.jpg', '34_9.jpg', '35_9.jpg', '36_11.jpg', '37_11.jpg', '38_11.jpg', '39_11.jpg', '3_10.jpg', '40_10.jpg', '41_10.jpg', '42_10.jpg', '43_10.jpg', '44_10.jpg', '45_10.jpg', '46_10.jpg', '47_10.jpg', '48_10.jpg', '49_10.jpg', '4_2.jpg', '50_10.jpg', '51_10.jpg', '5_2.jpg', '6_2.jpg', '7_2.jpg', '8_3.jpg', '9_3.jpg'];
+let distributedCards = [];
+
+// hit button
 let hitButton = document.querySelector('.hit-button');
 
-let distributedCards = [];
-let cardImgs = ['0_10.jpg', '10_3.jpg', '11_3.jpg', '12_4.jpg', '13_4.jpg', '14_4.jpg', '15_4.jpg', '16_5.jpg', '17_5.jpg', '18_5.jpg', '19_5.jpg', '1_10.jpg', '20_6.jpg', '21_6.jpg', '22_6.jpg', '23_6.jpg', '24_7.jpg', '25_7.jpg', '26_7.jpg', '27_7.jpg', '28_8.jpg', '29_8.jpg', '2_10.jpg', '30_8.jpg', '31_8.jpg', '32_9.jpg', '33_9.jpg', '34_9.jpg', '35_9.jpg', '36_11.jpg', '37_11.jpg', '38_11.jpg', '39_11.jpg', '3_10.jpg', '40_10.jpg', '41_10.jpg', '42_10.jpg', '43_10.jpg', '44_10.jpg', '45_10.jpg', '46_10.jpg', '47_10.jpg', '48_10.jpg', '49_10.jpg', '4_2.jpg', '50_10.jpg', '51_10.jpg', '5_2.jpg', '6_2.jpg', '7_2.jpg', '8_3.jpg', '9_3.jpg'];
-
 function hit() {
-  let newCardNum;
+  let cardNum;
   while (1) {
     // it's a 52 card deck
-    newCardNum = Math.floor(Math.random() * 52);
-    if (!distributedCards.includes(newCardNum)) {
+    cardNum = Math.floor(Math.random() * 52);
+    if (!distributedCards.includes(cardNum)) {
       break;
     }
   }
 
-  distributedCards.push(newCardNum);
+  distributedCards.push(cardNum);
 
-  let cardName = cardImgs[newCardNum];
+  let cardName = cardImgs[cardNum];
   let card = document.createElement('img');
   card.src = `/static/images/${cardName}`
 
@@ -29,3 +30,24 @@ function hit() {
 }
 
 hitButton.addEventListener('click', hit);
+
+// ###############################################################
+
+// stand button
+let standButton = document.querySelector('stand-button');
+
+function stand() {
+  let computerCards = document.querySelector('computer-cards');
+
+  let newCardNum
+  while (1) {
+    // it's a 52 card deck
+    newCardNum = Math.floor(Math.random() * 52);
+    if (!distributedCards.includes(newCardNum)) {
+      break;
+    }
+  }
+  
+}
+
+// standButton.addEventListener('click', stand);
